45a2cb1133d3375d7b5360ce0f83d15c29db75bf
Emrgul <emregul498@gmail.com>
2022-09-19 11:53:19 +0300

    javada veri tipleri uygulaması yapıldı.

---
+++ datatypes/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // java tip güvenli bir dildir.
+        // yani tanımladığın değişkenin tipini bilmek ister.
+
+        // primitive types :
+        // boolean , byte, short , int , long ,float double
+
+        int number = 12;
+
+        byte number2 = 10;
+
+        short number3 =-19999;
+
+        long number4 = 1111111;
+
+        double number5 = 12.6;
+
+        boolean dogruMu = true;
+
+        char deger = 'a';
+
+        String deger2 = "ankara";
+
+    }
+}
